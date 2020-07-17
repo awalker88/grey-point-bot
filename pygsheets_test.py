29eb191a4f5cd52b2e4eb2522880822a70301cc6
AndrewWalker <andrew.walker@ibm.com>
2020-07-16 22:18:33 -0500

    extra workbook print

--- pygsheets_test.py
+++ pygsheets_test.py
@@ -7,6 +7,8 @@ client = pyg.authorize(
     service_account_env_var='sheet_client_secret_json'
 )
 print(':::authorized')
+workbook = client.open('Grey Points')
+print('workbook:::', workbook)
 while True:
     workbook = client.open('Grey Points')
     print('workbook:::', workbook)
