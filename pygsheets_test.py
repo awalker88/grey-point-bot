bc1e8185102660f7cede0edfeb0e6d3dd361ed82
Andrew Walker <awalker88@me.com>
2020-07-16 22:05:06 -0500

    testing

--- pygsheets_test.py
+++ pygsheets_test.py
@@ -1,8 +1,13 @@
 import pygsheets as pyg
+from time import sleep
 
+
+print(':::started'
 client = pyg.authorize(
     service_account_env_var='sheet_client_secret_json'
 )
-
-workbook = client.open('Grey Points')
-print('workbook:::', workbook)
+print(':::authorized
+while True:
+    workbook = client.open('Grey Points')
+    print('workbook:::', workbook)
+    sleep(3)
