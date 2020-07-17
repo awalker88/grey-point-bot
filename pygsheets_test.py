1e7fdd6f8990da95081b5e46d817e02212c8d38a
Andrew Walker <awalker88@me.com>
2020-07-16 21:57:25 -0500

    switch to env json

--- pygsheets_test.py
+++ pygsheets_test.py
@@ -1,7 +1,7 @@
 import pygsheets as pyg
 
 client = pyg.authorize(
-    # service_account_env_var='sheet_client_secret_json' todo: for local
+    service_account_env_var='sheet_client_secret_json'
 )
 
 workbook = client.open('Grey Points')
