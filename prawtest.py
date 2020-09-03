e17c90313d4f353c59eaac886e927e0b73b8e144
AndrewWalker <andrew.walker@ibm.com>
2020-09-03 08:26:03 -0500

    remove unused files

--- prawtest.py
+++
@@ -1,5 +0,0 @@
-client_id='T3stYBR3jNU22A',
-client_secret='prNI-GIEzOQ5WfbXInbNVSGKoRM',
-user_agent='grey-point-bot v1',
-username='grey-point-bot',
-password='serk2SCIK.sirs'
--- pygsheets_test.py
+++
@@ -1,8 +0,0 @@
-import pygsheets as pyg
-
-client = pyg.authorize(
-    # service_account_env_var='sheet_client_secret_json' todo: for local
-)
-
-workbook = client.open('Grey Points')
-print('workbook:::', workbook)
