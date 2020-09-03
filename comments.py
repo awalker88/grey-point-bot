a03403c57088414213f894b68ff06a9f2fcd75af
AndrewWalker <andrew.walker@ibm.com>
2020-09-03 08:27:56 -0500

    comments.py now uses a main function

--- comments.py
+++ comments.py
@@ -6,6 +6,29 @@ import datetime
 from tqdm import tqdm
 from datetime import datetime
 
+
+def main():
+    dates = [int(get_time_stamp(date)) for date in get_date_ranges()]
+
+    cmmts = []
+    for i in tqdm(range(len(dates)-1)):
+        # Setting up dates
+        after = dates[i]
+        before = dates[i+1]
+
+        cmmts.append(get_user_comments('mindofmetalandwheels', 500, after=after, before=before))
+        time.sleep(0.5)
+
+    unrolled_cmmts = []
+    for cmmt in cmmts:
+        for c in cmmt:
+            unrolled_cmmts.append(c)
+
+    cmmts = pd.DataFrame(unrolled_cmmts, columns=['comment', 'link_id', 'id', 'time'])
+    cmmts['permalink'] = 'www.reddit.com/comments/' + cmmts['link_id'].str[3:] + '/_/' + cmmts['id']
+    cmmts.to_csv('cgp_grey_comments.csv', index=False)
+
+
 def get_date_ranges():
     dates_list = []
 
@@ -29,40 +52,21 @@ def get_date_ranges():
 
 
 def get_user_comments(user, size, after, before):
-    url = (f'https://api.pushshift.io/reddit/search/comment/?author={user}&sort=asc&size={size}&after={after}&before={before}')
+    url = f'https://api.pushshift.io/reddit/search/comment/?author={user}&sort=asc&size={size}&after={after}&before={before}'
     r = requests.get(url)
     try:
         raw_json = json.loads(r.text)
         data = raw_json['data']
-        comments = [[post['body'], post['link_id'], post['id'], datetime.utcfromtimestamp(post['created_utc'])] for post in data]
+        comments = [[post['body'], post['link_id'], post['id'], datetime.utcfromtimestamp(post['created_utc'])]
+                    for post in data]
     except json.decoder.JSONDecodeError:
         return []
     return comments
 
 
-def getTimeStamp(date_input):
+def get_time_stamp(date_input):
     return time.mktime(datetime.strptime(date_input, "%d/%m/%Y").timetuple())
 
 
-# comments = get_user_comments('awalker88', 10, int(getTimeStamp('1/4/2020')), int(getTimeStamp('27/4/2020')))
-
-
-dates = [int(getTimeStamp(date)) for date in get_date_ranges()]
-
-cmmts = []
-for i in tqdm(range(len(dates)-1)):
-    # Setting up dates
-    after = dates[i]
-    before = dates[i+1]
-
-    cmmts.append(get_user_comments('mindofmetalandwheels', 500, after=after, before=before))
-    time.sleep(0.5)
-
-unrolled_cmmts = []
-for cmmt in cmmts:
-    for c in cmmt:
-        unrolled_cmmts.append(c)
-
-cmmts = pd.DataFrame(unrolled_cmmts, columns=['comment', 'link_id', 'id', 'time'])
-cmmts['permalink'] = 'www.reddit.com/comments/' + cmmts['link_id'].str[3:] + '/_/' + cmmts['id']
-cmmts.to_csv('cgp_grey_comments.csv', index=False)
+if __name__ == '__main__':
+    main()
